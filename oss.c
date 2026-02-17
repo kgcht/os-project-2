d3539f1ff3f09d5f524948ceda27ae30aaceff61
Kayla Gaynor <kaylagaynor@Kaylas-MacBook-Air.local>
2026-02-17 11:06:00 -0600

    Simplified version

--- oss.c
+++ oss.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
 
 typedef struct {
 	int seconds;
@@ -21,7 +22,40 @@ int main() {
 
 	printf("OSS: Clock initialized to %d:%d\n", clock -> seconds, clock -> nanoseconds);
 
+	//Added fork and launch one worker
+	pid_t pid = fork();
+
+	if (pid < 0) {
+		perror("fork failed");
+		return 1;
+	}
+
+	else if (pid == 0) {
+		execl("./worker", "worker", "5", "500000", NULL);
+		perror("execl failed");
+		exit(1);
+	}
+
+	else {
+		printf("OSS: Launched worker PID %d\n", pid);
+
+		for (int i = 0; i < 10; i++) {
+			clock -> nanoseconds += 100000000;
+			if (clock -> nanoseconds >= 100000000) {
+				clock -> seconds++;
+				clock -> nanoseconds -= 100000000;
+			}
+		}
+
+		printf("OSS: Clock now at %d:%d\n", clock -> seconds, clock -> nanoseconds);
+
+		wait(NULL);
+		printf("OSS: Worker finished\n");
+
+	}
+
 	shmdt(clock);
 	shmctl(shmid, IPC_RMID, NULL);
+	printf("OSS: Cleaned up and terminating\n");
 	return 0;
 }
