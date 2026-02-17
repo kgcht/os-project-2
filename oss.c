a6997c6c279da8360eda9a9aba7bc801305d1f39
Kayla Gaynor <kaylagaynor@Kaylas-MacBook-Air.local>
2026-02-17 12:31:02 -0600

    Added getopt

--- oss.c
+++ oss.c
@@ -10,8 +10,52 @@ typedef struct {
 	int nanoseconds;
 } SystemClock;
 
-int main() {
-	printf("OSS: Starting PID:%d\n", getpid());
+void print_help(char *prog) {
+	printf("Usage: %s [-h] [-n proc] [-s simul] [-t timelimit] [-i interval]\n", prog);
+	printf("  -h	Show this help\n");
+	printf("  -n proc	Total processes to launch (default: 5)\n");
+	printf("  -s simul	Max simultaneos processes (default: 3)\n");
+	printf("  -t timelimit	Max time for each child in seconds (default: 1)\n");
+	printf("  -i interval	Launch interval in seconds (default: 0.1)\n");
+}
+
+int main(int argc, char *argv[]) {
+	int n = 5;
+	int s = 3;
+	double t = 1.0;
+	double i = 0.1;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "hn:s:t:i:")) != -1) {
+		switch (opt) {
+			case 'h':
+				print_help(argv[0]);
+				return 0;
+			case 'n':
+				n = atoi(optarg);
+				break;
+			case 's':
+				s = atoi(optarg);
+				break;
+			case 't':
+				t = atof(optarg);
+				break;
+			case 'i':
+				i = atof(optarg);
+				break;
+			default:
+				print_help(argv[0]);
+				return 1;
+
+		}
+	}
+
+	printf("OSS: Starting PID:%d PPID:%d\n", getpid(), getppid());
+	printf("Called with:\n");
+	printf("-n %d\n", n);
+	printf("-s %d\n", s);
+	printf("-t %.1f\n", t);
+	printf("-i %.1f\n", i);
 
 	key_t key = ftok(".", 'S');
 	int shmid = shmget(key, sizeof(SystemClock), IPC_CREAT | 0666);
@@ -31,7 +75,15 @@ int main() {
 	}
 
 	else if (pid == 0) {
-		execl("./worker", "worker", "5", "500000", NULL);
+		
+		int secs = (int)t;
+		int nanos = (int)((t - secs) * 1000000000);
+
+		char sec_str[20], nano_str[20];
+		snprintf(sec_str, sizeof(sec_str), "%d", secs);
+		snprintf(nano_str, sizeof(nano_str), "%d", nanos);
+
+		execl("./worker", "worker", sec_str, nano_str, NULL);
 		perror("execl failed");
 		exit(1);
 	}
@@ -39,15 +91,14 @@ int main() {
 	else {
 		printf("OSS: Launched worker PID %d\n", pid);
 
-		for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
 			clock -> nanoseconds += 100000000;
-			if (clock -> nanoseconds >= 100000000) {
+			if (clock -> nanoseconds >= 1000000000) {
 				clock -> seconds++;
-				clock -> nanoseconds -= 100000000;
+				clock -> nanoseconds -= 1000000000;
 			}
 		}
 
-		printf("OSS: Clock now at %d:%d\n", clock -> seconds, clock -> nanoseconds);
 
 		wait(NULL);
 		printf("OSS: Worker finished\n");
